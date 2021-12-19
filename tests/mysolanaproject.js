39b5a47d2229542484bf5ad896b8999728df9b34
Roberto Zahuis <zahuis314@gmail.com>
2021-12-19 07:50:40 -0500

    Add gifs to Account

--- tests/mysolanaproject.js
+++ tests/mysolanaproject.js
@@ -28,15 +28,19 @@ const main = async() => {
   console.log('👀 GIF Count', account.totalGifs.toString())
 	
   // Call add_gif!
-  await program.rpc.addGif({
+  await program.rpc.addGif("https://media.giphy.com/media/WeyqDp1shHe7u/giphy.gif", {
     accounts: {
       baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
     },
   });
   
   // Get the account again to see what changed.
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString())
+  
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
